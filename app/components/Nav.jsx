ad4fe80b4591c4d1422d0af4606eb42794cff88d
clayhero <theclayhero@gmail.com>
2016-08-07 15:27:40 -0400

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return (
+  <div className="top-bar">
+    <div className="top-bar-left">
+      <ul className="menu">
+        <li className="menu-text">React Timer App</li>
+        <li>
+          <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+        </li>
+        <li>
+          <Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+        </li>
+    </ul>
+    </div>
+    <div className="top-bar-right">
+      <p className="menu-text">Created by <a href="http://www.clayhero.com/about" target="_blank">Clay Walter</a></p>
+    </div>
+  </div>
+);
+}
+
+
+module.exports = Nav;
