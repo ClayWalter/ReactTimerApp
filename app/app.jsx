2771fb0409f882f31c4f0b5564ae538d6a4400a2
clayhero <theclayhero@gmail.com>
2016-08-28 18:22:03 -0400

    Support Foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,6 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // Load Foundaiton
-require('style!css!foundation-sites/dist/foundation.min.css'),
 $(document).foundation();
 
 // APP css
